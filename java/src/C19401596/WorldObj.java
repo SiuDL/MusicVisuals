0022eaf84bbc8d1b1e19479ca12f9466d9f588a9
SiuDzoen <C19401596@mytudublin.ie>
2021-04-14 18:01:38 +0100

    Updating WorldObj class

--- java/src/C19401596/WorldObj.java
+++ java/src/C19401596/WorldObj.java
@@ -5,11 +5,11 @@ public class WorldObj extends GameObject{
         super(game,0,0);
     }
 
-    private float floorH = game.height * 0.88f;
+    private float floorY = game.height * 0.88f;
 
     public void rendFloor(){
         game.fill(255);
-        game.rect(0, floorH, game.width, 100);
+        game.rect(0, floorY, game.width, 100);
     }
 
     public void render() {
@@ -21,6 +21,6 @@ public class WorldObj extends GameObject{
     }
 
     public float getFloor(){
-        return floorH;
+        return floorY;
     }
 }
